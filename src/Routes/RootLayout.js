eb3ba9271132b33ca90edda0d2505b28984f0247
Aayush Aryal <rinnegan2024@gmail.com>
2023-01-12 20:29:34 +0545

    added cart navlink

--- src/Routes/RootLayout.js
+++ src/Routes/RootLayout.js
@@ -1,4 +1,5 @@
 import { Outlet, NavLink } from "react-router-dom";
+import ShoppingCart from "../components/shopping-cart.svg";
 export default function RootLayout() {
   return (
     <div className="root-layout">
@@ -6,7 +7,12 @@ export default function RootLayout() {
         <nav>
           <h1>Sneaka Sneaker</h1>
           <NavLink to="/shop">Shop</NavLink>
-          <NavLink to="/checkout">Checkout</NavLink>
+          <NavLink to="/checkout">
+            <div className="cart-container">
+              <div className="cart-items">2</div>
+              <img src={ShoppingCart} className="shopping-cart" />
+            </div>
+          </NavLink>
           <NavLink to="/">Home</NavLink>
         </nav>
       </header>
