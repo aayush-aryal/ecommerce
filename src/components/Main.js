8f260229b4f62515dd4b787bb690def3a3a2fe17
Aayush Aryal <rinnegan2024@gmail.com>
2023-01-15 23:35:46 +0545

    added shoes, dependencies and styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
-import Sneaker from "../assets/homepage-sneaker.avif";
-import Sneaker2 from "../assets/homepage-personshoe.avif";
-import Sneaker3 from "../assets/homepage-personshoe2.avif";
 import ant from "../assets/anthony-edwards.jpg";
-export default function Main() {
+export default function Main(props) {
   return (
     <div className="main">
       <div className="parent-container">
@@ -15,39 +12,28 @@ export default function Main() {
           </div>
         </div>
       </div>
-      <h2>New Arrivals</h2>
-      <div className="image-info-container">
-        <div className="information">
-          <p>Sports</p>
-          <p>Casual</p>
-          <p>Collection</p>
+      <div className="featured">
+        <h2>Featured</h2>
+        <p>Star Player Edition</p>
+        <div className="shoe-container">
+          {props.sneakersArray.map((shoe) => (
+            <div className="single-shoe">
+              <Link to="/shop">
+                <img src={shoe.src} key={Math.random()} className="shoe" />
+              </Link>
+              <p>{shoe.name}</p>
+            </div>
+          ))}
         </div>
-        <div className="image-display">
-          <div className="indiv-imgcontainer">
-            <img src={Sneaker} alt="" className="display-images" />
-            <h2>Random</h2>
-            <p>Shop from our casual collection</p>
-          </div>
-          <div className="indiv-imgcontainer">
-            <img src={Sneaker2} className="display-images" alt="" />
-            <h2>Random</h2>
-            <p>Shop from our exclusive sportswear collection</p>
-          </div>
-          <div className="indiv-imgcontainer">
-            <img src={Sneaker3} className="display-images" alt="" />
-            <h2>Random</h2>
-            <p>Shop from our unique rep collection</p>
-          </div>
+      </div>
+      <div className="cover-athlete">
+        <div className="cover-promoinfo">
+          <h2>Gear Up This Season</h2>
+          <p>Look like a pro, play like a pro</p>
+          <p>Rep your favourite player's kick</p>
+          <button>Shop our player's collection bundle {">"} </button>
         </div>
-        <div className="cover-athlete">
-          <div className="cover-promoinfo">
-            <h2>Gear Up This Season</h2>
-            <p>Look like a pro , play like a pro</p>
-            <p>Rep your favourite player's kick</p>
-            <button>Shop our player's collection bundle > </button>
-          </div>
-          <img src={ant} className="anthony-edwards" alt="" />
-        </div>
+        <img src={ant} className="anthony-edwards" alt="" />
       </div>
     </div>
   );
